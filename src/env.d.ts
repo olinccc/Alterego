3aa052e08feee3e5e9d3ec639cb0893963dc051a
olinca <olinca.laguna@gmail.com>
2025-06-03 01:19:49 -0600

    segundo

---
+++ src/env.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="astro/client" />
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.JPG' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export default value;
+} 
