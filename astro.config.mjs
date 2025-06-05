655aa0f6cce9f0570120202ea57df9765808e05d
JuanFuent.es <juan.fuentes.cabrera.89@gmail.com>
2025-06-04 23:02:57 -0600

    Add deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
+  site: "https://olinccc.github.io/Alterego",
+  base: "/Alterego",
+  trailingSlash: "never",
+  outDir: "docs",
   vite: {
     plugins: [tailwindcss()]
+  },
+  typescript: {
+    strict: false,
+    check: false
   }
 });
